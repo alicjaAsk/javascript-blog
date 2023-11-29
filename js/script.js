b806f36970b71a99620bcaa6ed22804c25ea8fd3
Alicja SK <a.szmydkrysiak@gmail.com>
2023-11-29 05:56:40 +0100

    lists of titles

--- js/script.js
+++ js/script.js
@@ -12,11 +12,11 @@ const titleClickHandler = function (event) {
   console.log("link was clicked");
   event.preventDefault();
 
-  /* [DONE] remove class 'active' from all article links  */
+  /* remove class 'active' from all article links  */
   const activeLink = document.querySelector(".titles a.active");
   if (activeLink) activeLink.classList.remove("active");
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* add class 'active' to the clicked link */
   clickedElement.classList.add("active");
 
   /* remove class 'active' from all articles */
@@ -40,3 +40,45 @@ const links = document.querySelectorAll(".titles a");
 for (let link of links) {
   link.addEventListener("click", titleClickHandler);
 }
+
+const optArticleSelector = ".post",
+  optTitleSelector = ".post-title",
+  optTitleListSelector = ".titles";
+
+function generateTitleLinks(customSelector = "") {
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  let html = "";
+
+  /* for each article */
+  const articles = document.querySelectorAll(
+    optArticleSelector + customSelector
+  );
+  for (const article of articles) {
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+    /*  find the title element */
+    /*  get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /*  create HTML of the link */
+    // const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    const linkHTMLData = { id: articleId, title: articleTitle };
+    const linkHTML = templates.articleLink(linkHTMLData);
+    console.log("linkHTML: ", linkHTML);
+    /*  insert link into html variable */
+    html = html + linkHTML;
+  }
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll(".titles a");
+  console.log("link: ", links);
+
+  for (const link of links) {
+    link.addEventListener("click", titleClickHandler);
+  }
+}
+
+generateTitleLinks();
+
+// function clearMessages(){
+// 	document.getElementById('messages').innerHTML = '';
+// }
